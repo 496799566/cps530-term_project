9d7bcefcdabf7fa4b091932ccf14fb02b878de92
KichiroM <40420641+KichiroM@users.noreply.github.com>
2018-11-24 16:10:03 -0500

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ContributionComponent } from './contribution/contribution.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { HomeComponent } from './home/home.component';
+import { ExampleComponent } from './example/example.component';
+import { InstallComponent } from './install/install.component';
+import { AboutComponent } from './about/about.component';
+import { TutorialComponent } from './tutorial/tutorial.component';
+
+const routes: Routes = [
+    { path: 'install', component: InstallComponent },
+    { path: 'example', component: ExampleComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'experience', component: ExperienceComponent },
+    { path: 'contribution', component: ContributionComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'tutorial', component: TutorialComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
